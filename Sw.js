50b5021b9bbd7cce19b620fdabe86fafcc3be289
rezapahlavaniiiii-netizen <rezapahlavaniiiii@gmail.com>
2026-07-11 01:26:52 +0330

    Create Sw.js

---
+++ Sw.js
@@ -0,0 +1,16 @@
+onst CACHE_NAME = 'expense-app-v3';
+const urlsToCache = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache)));
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+    e.waitUntil(caches.keys().then(names => Promise.all(names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n)))));
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
