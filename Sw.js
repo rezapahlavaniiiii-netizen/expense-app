ae7be7e0c2f437c41caa15a958ac9511400c49b7
rezapahlavaniiiii-netizen <rezapahlavaniiiii@gmail.com>
2026-07-11 16:55:48 +0330

    Update Sw.js

--- Sw.js
+++ Sw.js
@@ -20,25 +20,3 @@ self.addEventListener('activate', function(event) {
         }));
     }));
 });
-var CACHE_NAME = 'finance-v3';
-var urlsToCache = ['./', './index.html', './manifest.json'];
-
-self.addEventListener('install', function(event) {
-    event.waitUntil(caches.open(CACHE_NAME).then(function(cache) {
-        return cache.addAll(urlsToCache);
-    }));
-});
-
-self.addEventListener('fetch', function(event) {
-    event.respondWith(caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-    }));
-});
-
-self.addEventListener('activate', function(event) {
-    event.waitUntil(caches.keys().then(function(names) {
-        return Promise.all(names.map(function(name) {
-            if (name !== CACHE_NAME) return caches.delete(name);
-        }));
-    }));
-});
